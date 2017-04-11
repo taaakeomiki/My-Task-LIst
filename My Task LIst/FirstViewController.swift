952e9e315138b9a4702fd856f0306f5c3dffd335
三木健生 <mikitakeo@ikuta-mobile194-92.mind.meiji.ac.jp>
2017-04-11 13:13:00 +0900

    Initial Commit

---
+++ My Task LIst/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  My Task LIst
+//
+//  Created by 三木健生 on 2017/04/11.
+//  Copyright © 2017年 mikitakeo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ My Task LIst/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  My Task LIst
+//
+//  Created by 三木健生 on 2017/04/11.
+//  Copyright © 2017年 mikitakeo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
