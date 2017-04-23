111ca151fcd4658721db9baef90b9a70b1cedda6
taaakeomiki <ee41236@meiji.ac.jp>
2017-04-23 15:58:08 +0900

    To-do-List
    to-do-list

--- My Task LIst/SecondViewController.swift
+++ My Task LIst/SecondViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet var textTask: UITextField!
+    @IBOutlet var textDesc: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,30 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func btnAddTask_Click(sender: UIButton){
+        //print("the button was ckicled.")
+        taskMgr.addTask(name: textTask.text!, desc: textDesc.text!);
+        self.view.endEditing(true)
+        textTask.text = ""
+        textDesc.text = ""
+        self.tabBarController?.selectedIndex = 0;
+        
+    }
+    
+    
+    
+    
+    
+    //override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!){
+      //  self.view.endEditing(true)
+    //}
+    
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        textField.resignFirstResponder();
+        return true
+    }
 
 }
 
